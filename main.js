b9717b6951d210f9564de8d0585bb5dc4dbd856e
Miroslav Repka <m.repka@lancaster.ac.uk>
2021-03-10 10:52:55 +0000

    Adding some database lines

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
 const config = require('./config/config.js');
 const client = new Discord.Client();
-const firebse = require('./node_modules/firebase/app');
+// database stuff
+const firebase = require('./node_modules/firebase/app');
 const FieldValue = require('./node_modules/firebase-admin').firestore.FieldValue;
 const admin = require('./node_modules/firebase-admin');
 const serviceAccount = require('./config/serviceAccount.json');
-
-var currentOpponent = new Set();
-var startTime = new Map();
-
+//fighting buffers
+let currentOpponent = new Set();
+let startTime = new Map();
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -19,6 +19,12 @@ let db = admin.firestore();
 client.once('ready', () => {
     console.log('Preparing for the fight...');
     console.log('Delay:', config.delay);
+
+});
+
+client.on("guildCreate", guild => {
+    console.log("Joined a new guild: " + guild.name);
+    checkIfDbExists(client.guilds);
 });
 
 //When someone sends a message, try to fight him
@@ -30,7 +36,6 @@ client.on('message', function (message) {
 
 //check if fight message had a reaction
 client.on("messageReactionAdd", function (messageReaction, user) {
-    console.log(user + '\t' + currentOpponent.has(user) + '\t' + currentOpponent.size);
     if (currentOpponent.has(user))
         checkIfFoughtBack(messageReaction, user);
 });
@@ -48,15 +53,16 @@ process.on('SIGINT', function () {
 //-------------------functions-----------------------
 
 function figthMe(message) {
-    var fightProbability = Math.random();
+    let fightProbability = Math.random();
     //dont react to own message
-    if (message.author != client.user) {
+    if (message.author !== client.user) {
         if (fightProbability > 0.5) {
-            console.log('Fighting' + message.author);
             currentOpponent.add(message.author);
             message.channel.send('Fight me <@' + message.member.id + '>!');
-            startTime = new Date().getTime();
-            setTimeout(function () { checkIfLost(message.author, message); }, (config.delay) * 1000);
+            startTime.set(message.author, new Date().getTime());
+            setTimeout(function () {
+                checkIfLost(message.author, message);
+            }, (config.delay) * 1000);
         }
     }
 }
@@ -66,17 +72,15 @@ function checkIfFoughtBack(messageReaction, user) {
     if (messageReaction.message.content.includes('Fight me')) {
         if (currentOpponent.has(user)) {
             if (messageReaction.emoji.identifier === '%F0%9F%A4%BA') {
-                if (new Date().getTime() - (config.delay * 1000) <= startTime) {
+                if (new Date().getTime() - (config.delay * 1000) <= startTime.get(user)) {
                     won(messageReaction, user);
-                    currentOpponent.delete(user);
+                    restart(user);
                 } else {
                     lostBecauseTime(messageReaction.message);
-                    currentOpponent.delete(user);
                 }
             } else {
                 console.log("Miss");
                 lostBecauseMissClick(messageReaction, user);
-                currentOpponent.delete(user);
             }
         }
     }
@@ -92,21 +96,20 @@ function checkIfLost(opponent, message) {
     console.log('Opponent', opponent, '\n if ', currentOpponent.has(opponent));
     if (currentOpponent.has(opponent) && opponent != null) {
         lostBecauseTime(message, opponent);
-        currentOpponent.delete(opponent);
     }
 }
 
 function lostBecauseTime(message, opponent) {
     message.channel.send('<@' + opponent.id + '> was too slow!');
     setNicknameTo(message.guild.me, 'Coward', opponent);
-    restart();
+    restart(opponent);
 }
 
 function lostBecauseMissClick(messageReaction, user) {
     console.log('Wrong emoji');
     messageReaction.message.channel.send('<@' + user.id + '> is that a missclick? Looks like you will be a coward...');
     setNicknameTo(messageReaction.message.guild.me, 'Coward', user);
-    restart();
+    restart(user);
 }
 
 function won(messageReaction, user) {
@@ -114,9 +117,24 @@ function won(messageReaction, user) {
     messageReaction.message.channel.send('<@' + user.id + '> is not a coward!');
 }
 
-function restart() {
+function restart(user) {
     console.log('Restarting');
-    startTime = 0;
+    startTime.delete(user);
+    currentOpponent.delete(user);
+}
+
+function checkIfDbExists(guild) {
+    const querySnapshot = admin.firestore().collection('guilds').doc('guild-' + guild.id).collection('subcollection').get().then(querySnapshot => {
+        //table already exists
+        console.log(`Found document`);
+    }).catch(async documentSnapshot => {
+        //create a table for this guild
+        const data = {
+            name: guild.name,
+            id: guild.id
+        };
+        const res = await db.collection('guilds').doc('guild-' + guild.id).set(data);
+    });
 }
 
 client.login(config.token);
